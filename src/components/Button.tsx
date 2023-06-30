91434d3764608bc6ef294477fa60897d45d0af91
winnicius-moura <winnicius.moura@gmail.com>
2023-06-30 10:51:45 -0300

    essencials local test ok

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 export interface IButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-  backgroundColor?: string
-  color?: string
+  backgroundColor?: string;
+  color?: string;
 }
 
 export const Button: React.FunctionComponent<IButtonProps> = (props) => {
-  const { children, backgroundColor, color, style } = props
+  const { children, backgroundColor, color, style } = props;
 
-  let defaultStyle: React.CSSProperties = style || {}
+  let defaultStyle: React.CSSProperties = style || {};
 
-  if (backgroundColor) defaultStyle.backgroundColor = backgroundColor
-  if (color) defaultStyle.color = color
+  if (backgroundColor) defaultStyle.backgroundColor = backgroundColor;
+  if (color) defaultStyle.color = color;
 
   return (
     <button style={defaultStyle} {...props}>
       {children}
     </button>
-  )
+  );
 };
