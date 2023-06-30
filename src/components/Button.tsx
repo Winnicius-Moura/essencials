bf0fd352a2c97c3d49a524813d7d05e88d0cd24a
winnicius-moura <winnicius.moura@gmail.com>
2023-06-30 10:25:27 -0300

    feat: first button

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export interface IButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  backgroundColor?: string
+  color?: string
+}
+
+export const Button: React.FunctionComponent<IButtonProps> = (props) => {
+  const { children, backgroundColor, color, style } = props
+
+  let defaultStyle: React.CSSProperties = style || {}
+
+  if (backgroundColor) defaultStyle.backgroundColor = backgroundColor
+  if (color) defaultStyle.color = color
+
+  return (
+    <button style={defaultStyle} {...props}>
+      {children}
+    </button>
+  )
+};
