1c89c6be728f7eefff9d9ea5a7c4527e660a4927
winnicius-moura <winnicius.moura@gmail.com>
2023-09-04 19:12:33 -0300

    add: ValidateUtils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,4 +7,24 @@ export class ArrayUtils {
     }
     return false
   }
+
+  static checkEqualsFromArrays = (arrayA: any[], arrayB: any[]) => arrayA?.some(r => arrayB?.indexOf(r) >= 0)
+}
+
+export class ValidateUtils {
+  static isValidCPF(cpf: string) {
+
+    cpf = cpf.replace(/[^\d]+/g, '')
+
+    if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) {
+      return false
+    }
+
+    const validateCPF = cpf.split('').map(el => +el)
+
+    const rest = (count: number) => (validateCPF.slice(0, count - 12))
+      .reduce((soma, el, index) => (soma + el * (count - index)), 0) * 10 % 11 % 10
+
+    return rest(10) === validateCPF[9] && rest(11) === validateCPF[10]
+  }
 }
