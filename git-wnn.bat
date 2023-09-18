21e2f36ef38e93032ae9faa7a6079f1d70df609d
winnicius-moura <winnicius.moura@gmail.com>
2023-09-18 10:17:37 -0300

    add: pull request in script"

--- git-wnn.bat
+++ git-wnn.bat
@@ -9,9 +9,14 @@ pushd %~dp0
   echo wnn.dev
   set /p commit_message=Digite a mensagem do commit: 
   set /p origin_name=Digite o nome da origin: 
+  set /p base_branch=Digite o nome da branch de destino: 
+  set /p head_branch=Digite o nome da sua branch local: 
+  set /p pr_title=Digite o título do Pull Request: 
+  set /p pr_description=Digite a descrição do Pull Request: 
   git add .
   git commit -m "%commit_message%"
   git push origin "%origin_name%"
+  gh pr create --base "%base_branch%" --head "%head_branch%" --title "%pr_title%" --body "%pr_description%"
   
 
 %@EndTry%
