5ae54b6fc6bfb06b5ad3d6ece3c82549407a622d
winnicius-moura <winnicius.moura@gmail.com>
2023-09-18 10:45:24 -0300

    fix: wnn.bat script

--- git-wnn.bat
+++ git-wnn.bat
@@ -3,19 +3,19 @@ pushd %~dp0
 %@Try%
   REM 
 
-  echo automate commit and push
+  echo automate commit and pull request
 
   echo __________________________________________
   echo wnn.dev
   set /p commit_message=Digite a mensagem do commit: 
-  set /p origin_name=Digite o nome da origin: 
+  @REM set /p origin_name=Digite o nome da origin: 
   set /p base_branch=Digite o nome da branch de destino: 
   set /p head_branch=Digite o nome da sua branch local: 
   set /p pr_title=Digite o titulo do Pull Request: 
   set /p pr_description=Digite a descricao do Pull Request: 
   git add .
   git commit -m "%commit_message%"
-  git push origin "%origin_name%"
+  @REM git push origin "%origin_name%"
   gh pr create --base "%base_branch%" --head "%head_branch%" --title "%pr_title%" --body "%pr_description%"
   
 
