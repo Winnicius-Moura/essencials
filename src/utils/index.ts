be7007ff21b4660bd8a81e8b4e65d4d0486b4ec6
winnicius-moura <winnicius.moura@gmail.com>
2023-11-22 20:46:07 -0300

    feat: new functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,6 +9,50 @@ export class ArrayUtils {
   }
 
   static checkEqualsFromArrays = (arrayA: any[], arrayB: any[]) => arrayA?.some(r => arrayB?.indexOf(r) >= 0)
+
+}
+
+export class ValueUtils {
+  static formatValuePtBr(value: number): string {
+    const formattedValue = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    })
+    return formattedValue
+  }
+}
+
+export class SearchUtils {
+  static findItem<T>(
+    data: T[],
+    fields: (keyof T)[],
+    searchValue: string | number
+  ): NonNullable<T> | undefined {
+    const lowerCaseSearchValue = String(searchValue).toLowerCase();
+
+    const foundItem = data.find(item =>
+      fields.some(field =>
+        String(item[field]).toLowerCase().includes(lowerCaseSearchValue)
+      )
+    );
+
+    return foundItem ? foundItem : undefined;
+
+  }
+}
+
+export class StringUtils {
+  static capitalizeWords(sentence: string): string {
+    const words = sentence.split(' ');
+
+    const capitalizedWords = words.map(word => {
+      const firstLetter = word.charAt(0).toUpperCase();
+      const restOfWord = word.slice(1).toLowerCase();
+      return firstLetter + restOfWord;
+    });
+
+    return capitalizedWords.join(' ');
+  }
 }
 
 export class ValidateUtils {
