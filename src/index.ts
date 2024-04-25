f2fa669ea383ec9596afc34c39bcf00be1e8943a
winnicius-moura <winnicius.moura@gmail.com>
2024-04-24 22:34:49 -0300

    fix: imports utils

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './components'
-export * from './utils/index'
+export * from './utils'
+export { CookieUtils, ArrayUtils, SearchUtils, StringUtils, ValidateUtils, ValueUtils, } from './utils/index';
