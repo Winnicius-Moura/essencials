be50e80139289543693da9cbeaa44c88fade406a
winnicius-moura <winnicius.moura@gmail.com>
2024-04-24 22:03:55 -0300

    formteste

---
+++ src/components/Forms.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+interface User {
+  uuid: string,
+  name: string,
+  email: string,
+}
+
+
+
+export const FormsComponent = ({ uuid, name, email }: User) => {
+
+
+
+  return (
+    <div>
+      
+
+    </div>)
+}
